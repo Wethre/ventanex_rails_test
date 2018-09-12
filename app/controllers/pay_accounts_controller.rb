8f10a402745dc11f4574a4e056d6ec3b5e1eb4a1
Thom Meas <Thom.meas@gmail.com>
2018-09-12 15:19:11 -0500

    added new template to create new account, created users table and add navbar

--- app/controllers/pay_accounts_controller.rb
+++ app/controllers/pay_accounts_controller.rb
@@ -7,10 +7,28 @@ class PayAccountsController < ApplicationController
   def show
   end
 
+  def new
+    @pay_account = PayAccount.new
+  end
+
   def create
+    @pay_account = PayAccount.new(account_params)
+    if @pay_account.save
+      flash[:success] = "Account was created successfully"
+      redirect_to pay_accounts_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
   end
+
+  private
+    def account_params
+      params.require(:pay_account).permit(:nickname, :account_number, :account_type)
+    end
+
+
 
 end
